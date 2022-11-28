6cb42e2602776364124d8a04f84f646a3e0cf907
Mateusz <dsw46476@student.dsw.edu.pl>
2022-11-28 20:54:23 +0100

    Excel_v4

--- BankAccount/src/Excel.java
+++ BankAccount/src/Excel.java
@@ -1,77 +1,78 @@
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 
-
 public class Excel {
+    static File file = new File("Plik_bankowy.xlsx");//instancja scieżki pliku
+    static String[][] dataTable = null;//tablica dwuwymiarowa typu String
+
+
+    public static void Read(String path) throws IOException {
+//        File file = new File(path);
+
+    }
+    public static void convertToString(String dataTable [][]){
+
+        for (int n = 0; n < dataTable.length; n++ )
+        {
+            System.out.println("------------------");
+            System.out.println(Arrays.deepToString(dataTable[n]));
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-
-        File file = new File("Plik_bankowy.xlsx");
-        String[][] dataTable = null;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-
- // public static void Read(String path) {
         try {
-            //File file = new File(path);
-            DataFormatter dataFormatter = new DataFormatter();
-
-//creating a new file instance
-            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
-// creating Workbook instance that refers to .xlsx file
-            XSSFWorkbook wb = new XSSFWorkbook(fis);
-            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
-            Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+            FileInputStream fis = new FileInputStream(file);//tworzenie nowej instancjii
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
+            DataFormatter dataFormatter = new DataFormatter();//jedna z funkcjonalności apache poi, która zawiera metody służące do formatowania wartości w komórkach pliku
+            XSSFSheet sheet = workbook.getSheetAt(0);//tworzy obiekt do pobierania na którym pracujemy
+            Iterator<Row> itr = sheet.iterator();//iteruje po całym pliku
             while (itr.hasNext())
             {
                 Row row = itr.next();
-                Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
+                Iterator<Cell> cellIterator = row.cellIterator();//iterowanie po każdej komórce
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
                     String cellValue = dataFormatter.formatCellValue(cell);
                     switch (cell.getCellType())
                     {
-                        case Cell.CELL_TYPE_STRING:    //field that represents string cell type
-                            System.out.print(cellValue + "\t\t\t");
+                        case Cell.CELL_TYPE_STRING:    //reprezentuje komorki typu String
                             break;
-                        case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type
-                            System.out.print(cellValue + "\t\t\t");
+                        case Cell.CELL_TYPE_NUMERIC:    //reprezentuje komurki typu numerycznego
                             break;
                         default:
                     }
-
                 }
-                System.out.println("");
             }
-            System.out.println("__________________");
             int numRows = sheet.getLastRowNum() +1;
             int numCols = sheet.getRow(0).getLastCellNum();
-
+//pętla for do wyciągnicia wszystkich danych i zapisania w tablicy dwuwymiarowej
             dataTable = new String[numRows][numCols];
             for (int i = 0; i < numRows; i++) {
                 XSSFRow xlRow = sheet.getRow(i);
                 for (int j = 0; j < numCols; j++) {
                     XSSFCell xlCell = xlRow.getCell(j);
                     dataTable[i][j] = xlCell.toString();
-
                 }
             }
-            System.out.println(Arrays.deepToString(dataTable));
-
         }
         catch(Exception e) {
             e.printStackTrace();
         }
 
+        convertToString(dataTable);
+
+
     }
-
-
 }
