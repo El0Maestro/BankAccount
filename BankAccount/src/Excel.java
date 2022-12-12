e50eac693103b19b61626599ce1ec1bd1a00b5b0
Mateusz <dsw46476@student.dsw.edu.pl>
2022-12-12 21:17:33 +0100

    Excel_v5

--- BankAccount/src/Excel.java
+++ BankAccount/src/Excel.java
@@ -1,5 +1,4 @@
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -13,28 +12,18 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class Excel {
-    static File file = new File("Plik_bankowy.xlsx");//instancja scieżki pliku
-    static String[][] dataTable = null;//tablica dwuwymiarowa typu String
+    File file = new File("Plik_bankowy.xlsx");//instancja scieżki pliku
+    String[][] dataTable;//tablica dwuwymiarowa typu String
+    FileInputStream fis;//tworzenie nowej instancjii
+    XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
 
-
-    public static void Read(String path) throws IOException {
-//        File file = new File(path);
-
-    }
-    public static void convertToString(String dataTable [][]){
-
-        for (int n = 0; n < dataTable.length; n++ )
-        {
-            System.out.println("------------------");
-            System.out.println(Arrays.deepToString(dataTable[n]));
-        }
+    public Excel() throws IOException {
     }
 
-    public static void main(String[] args) throws Exception {
+    void Read(String path) throws IOException {
+        this.file = new File(path);
         try {
-            FileInputStream fis = new FileInputStream(file);//tworzenie nowej instancjii
-            XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
-            DataFormatter dataFormatter = new DataFormatter();//jedna z funkcjonalności apache poi, która zawiera metody służące do formatowania wartości w komórkach pliku
+            //XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
             XSSFSheet sheet = workbook.getSheetAt(0);//tworzy obiekt do pobierania na którym pracujemy
             Iterator<Row> itr = sheet.iterator();//iteruje po całym pliku
             while (itr.hasNext())
@@ -44,15 +33,6 @@ public class Excel {
                 while (cellIterator.hasNext())
                 {
                     Cell cell = cellIterator.next();
-                    String cellValue = dataFormatter.formatCellValue(cell);
-                    switch (cell.getCellType())
-                    {
-                        case Cell.CELL_TYPE_STRING:    //reprezentuje komorki typu String
-                            break;
-                        case Cell.CELL_TYPE_NUMERIC:    //reprezentuje komurki typu numerycznego
-                            break;
-                        default:
-                    }
                 }
             }
             int numRows = sheet.getLastRowNum() +1;
@@ -70,9 +50,15 @@ public class Excel {
         catch(Exception e) {
             e.printStackTrace();
         }
-
         convertToString(dataTable);
 
+    }
+    static void convertToString(String dataTable [][]){
 
+        for (int n = 0; n < dataTable.length; n++ )
+        {
+            System.out.println("------------------");
+            System.out.println(Arrays.deepToString(dataTable[n]));
+        }
     }
 }
