23cf3f2b7e59b69b4fa7581a10bbeaaccbcb349e
Mateusz <dsw46476@student.dsw.edu.pl>
2023-01-12 17:31:38 +0100

    Excel V5

--- BankAccount/src/Excel.java
+++ BankAccount/src/Excel.java
@@ -1,3 +1,4 @@
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -11,31 +12,26 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 
-public class Excel {
-    File file = new File("Plik_bankowy.xlsx");//instancja scieżki pliku
-    String[][] dataTable;//tablica dwuwymiarowa typu String
-    FileInputStream fis;//tworzenie nowej instancjii
-    XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
+class Excel {
+    private File file = new File("Plik_bankowy.xlsx");//instancja scieżki pliku
+    private String[][] dataTable;//tablica dwuwymiarowa typu String
+    private FileInputStream fis;//tworzenie nowej instancjii
 
-    public Excel() throws IOException {
-    }
 
     void Read(String path) throws IOException {
         this.file = new File(path);
         try {
-            //XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);//odwołanie do pliku .xls
             XSSFSheet sheet = workbook.getSheetAt(0);//tworzy obiekt do pobierania na którym pracujemy
             Iterator<Row> itr = sheet.iterator();//iteruje po całym pliku
-            while (itr.hasNext())
-            {
+            while (itr.hasNext()) {
                 Row row = itr.next();
                 Iterator<Cell> cellIterator = row.cellIterator();//iterowanie po każdej komórce
-                while (cellIterator.hasNext())
-                {
+                while (cellIterator.hasNext()) {
                     Cell cell = cellIterator.next();
                 }
             }
-            int numRows = sheet.getLastRowNum() +1;
+            int numRows = sheet.getLastRowNum() + 1;
             int numCols = sheet.getRow(0).getLastCellNum();
 //pętla for do wyciągnicia wszystkich danych i zapisania w tablicy dwuwymiarowej
             dataTable = new String[numRows][numCols];
@@ -46,19 +42,21 @@ public class Excel {
                     dataTable[i][j] = xlCell.toString();
                 }
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         convertToString(dataTable);
 
     }
-    static void convertToString(String dataTable [][]){
 
-        for (int n = 0; n < dataTable.length; n++ )
-        {
+    void convertToString(String[][] dataTable) {
+
+        for (int n = 0; n < dataTable.length; n++) {
             System.out.println("------------------");
             System.out.println(Arrays.deepToString(dataTable[n]));
         }
+
     }
+
+
 }
--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,7 +1,13 @@
+
+
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Excel read = new Excel();
+
+        
+
 
     }
 }
